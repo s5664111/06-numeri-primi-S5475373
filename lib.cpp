cbee08dac49aedfce3661a7207c901224774120a
AndreaDotta <117829484+S5475373@users.noreply.github.com>
2022-12-02 21:53:47 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+bool namespace (int n) {
+  int i=1, x;
+  bool risultato;
+  if (n>1){
+    do {
+      i++;
+      x=n%i;
+    }
+    while (x!=0);
+    if (n==i) {
+      risultato = 0;
+      return risultato;
+    }
+    else {
+      risultato =1;
+      return risultato;
+    }
+  }
+}
