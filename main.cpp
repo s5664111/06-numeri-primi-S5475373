3ac4708d9f557d11480517bc8082bd2f9630bf1f
AndreaDotta <117829484+S5475373@users.noreply.github.com>
2022-12-02 21:57:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+usingnamespace std;
 
 int main(){
+  int p;
+  cin >> p;
+  if (numeroprimo(p) == 0) {
+    cout << "numero primo" << endl;
+  }
+  else {
+    cout << "numero non primo" << endl;
+  }
   return 0;
 }
