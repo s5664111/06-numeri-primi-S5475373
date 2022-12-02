6e9ce18350f014227f9280c5e37a91ff67c8ad05
s5664111 <117274748+s5664111@users.noreply.github.com>
2022-12-02 19:35:22 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
-
+#include "lib.h"
+bool primi(int n){
+  int div=1, conta=0;
+  while(conta<=1 && div<=n/2{
+    if(n%div==0){
+      conta++;
+    }
+    div++;
+  }
+        if (conta==1);
+        {
+return true;
+        }else{
+          return false;
+        }
+        }
