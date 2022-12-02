6ca2afccca8873a3c31d92fa1b19c7ac44140857
s5664111 <117274748+s5664111@users.noreply.github.com>
2022-12-02 19:37:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  int a;
+  bool b;
+  cin>>a;
+  b=primi(a);
+  if(b==true);
+  cout<<"numero primo"<<endl;
+  else if(b==false);
+  cout<<"numero non primo"<<endl;
+
   return 0;
 }
